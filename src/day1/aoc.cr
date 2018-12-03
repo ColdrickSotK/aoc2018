cd415750466a2d08a425be2f3d39fe78764a040c
Adam Coldrick <adam.coldrick@codethink.co.uk>
2018-12-03 14:45:53 +0000

    Day 1 part 2 solution

--- src/day1/aoc.cr
+++ src/day1/aoc.cr
@@ -1,13 +1,42 @@
 require "file"
 
-def load(path : String)
-  content = File.open(path) do |file|
-    file.gets_to_end
+module Day1
+  def load(path : String)
+    content = File.open(path) do |file|
+      file.gets_to_end
+    end
+    return content.strip().split("\n").map do |val|
+      val.to_i
+    end
   end
-  return content.strip().split("\n").map do |val|
-    val.to_i
+
+  def part1(data : Array)
+    return data.sum 0
+  end
+
+  def part2(data : Array)
+    seen = [] of Int32
+    current = 0
+    idx = 0
+    found = false
+
+    while true
+      current += data[idx]
+      if seen.includes? current
+        return current
+      else
+        seen << current
+      end
+      idx += 1
+      if idx >= data.size
+        idx = 0
+      end
+    end
   end
 end
 
+include Day1
+
 data = load(ARGV[0])
-puts data.sum 0
+puts part1 data
+puts part2 data
